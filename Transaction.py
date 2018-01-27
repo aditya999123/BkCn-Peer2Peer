245ec2822dccdc694826ba9f27f64e0252e7948d
aditya <aditya999123@gmail.com>
2018-01-27 11:11:13 +0530

    meged branch aditya | initial phase communication testing done

---
+++ Transaction.py
@@ -0,0 +1,28 @@
+import ecdsa, hashlib
+
+txnBuffer = []
+
+class Txn:
+	def __init__(self, senderPublicKey, recieverPublicKey, product, txnHash):
+		self.sender = senderPublicKey
+		self.reciever = recieverPublicKey
+		self.product = product
+		self.txnHash = txnHash
+		self.nonce = None
+
+		self.authenticated = self.authenticate(senderPublicKey, recieverPublicKey, txnHash)
+		self.validated = self.validate(self.sender, self.product)
+
+	def authenticate(senderPubKey, recieverPubKey, product_hash, hashed):
+		vk = ecdsa.VerifyingKey.from_string(senderPubKey.decode('hex'), curve=ecdsa.SECP256k1)
+		return vk.verify(hashed.decode('hex'), senderPubKey + recieverPubKey + product_hash)
+
+	def validate(sender, product):
+		owner = {}
+		for block in chain.blocks:
+			for txn in block.txns:
+				if(txn.product == product):
+					owner = txn.reciever
+		if(owner == sender):
+			return True
+		return False
